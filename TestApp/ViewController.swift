7000438382065ff4e3a18ae4211e10afd3b86a32
Shah Newaz Khan <shah@Shahs-Mac-Pro.local>
2015-08-24 10:11:01 -0700

    Initil Commit

--- TestApp/ViewController.swift
+++ TestApp/ViewController.swift
@@ -12,12 +12,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let sv = UIScrollView(frame: self.view.bounds)
+        sv.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+        //sv.pagingEnabled = true
+        
+        self.view.addSubview(sv)
+        sv.frame.origin = CGPointMake(30,30)
+        sv.backgroundColor = UIColor.whiteColor()
+        
+        var x : CGFloat = 10
+        
+        for i in 0 ..< 30 {
+            let lab = UILabel()
+            lab.text = "This is label \(i+1)"
+            lab.sizeToFit()
+            lab.frame.origin = CGPointMake(x,10)
+            sv.addSubview(lab)
+            x += lab.bounds.size.width + 10
+        }
+        
+        var sz = sv.bounds.size
+        
+        sz.width = x
+        sv.contentSize = sz //*
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
